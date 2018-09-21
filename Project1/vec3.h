ab7c4450c8011ba7e551386203cf7ffaa2337aab
VictorTirado <arlaquim@gmail.com>
2018-09-21 13:23:10 +0200

    Update vec3.h

--- Project1/vec3.h
+++ Project1/vec3.h
@@ -3,42 +3,24 @@
 
 using namespace std;
 
-template <class T>
+template <class Type_>
 class Vec3
 {
 public:
-	T x, y, z;
+	Type_ x, y, z;
 
 	Vec3() {};
-	Vec3(T x, T y, T z)
-	{
-		this->x = x;
-		this->y = y;
-		this->z = z;
-	}
-	Vec3(const Vec3 &v)
-	{
-		this->x = v.x;
-		this->y = v.y;
-		this->z = v.z;
-	};
+	Vec3(Type_ x, Type_ y, Type_ z) :x(x),y(y),z(z){}
+	Vec3(const Vec3 &v2): x(v.x),y(v.y),z(v.z){}
 	
 
-	Vec3 operator +(Vec3 &v2)
+	Vec3 operator +(Vec3 &v2) const
 	{
-		Vec3 result;
-		result.x = x + v2.x;
-		result.y = y + v2.y;
-		result.z = z + v2.z;
-		return result;
+		return Vec3(x + v2.x,y + v2.y, z + v2.z);
 	}
-	Vec3 operator -(Vec3 &v2)
+	Vec3 operator -(Vec3 &v2) const
 	{
-		Vec3 result;
-		result.x = x - v2.x;
-		result.y = y - v2.y;
-		result.z = z - v2.z;
-		return result;
+		return Vec3(x - v2.x,y - v2.y, z - v2.z);
 	}
 	Vec3 operator +=(Vec3 &v2)
 	{
@@ -54,20 +36,17 @@ public:
 		y -= v2.y;
 		z -= v2.z;
 
-			return(*this);
+		return(*this);
 	}
 	bool operator ==(Vec3 &v2)
 	{
-		if (x == v2.x && y == v2.y && z == v2.z)
-			return true;
-		else
-			return false;		
+		return (x == v2.x && y == v2.y && z == v2.z);	
 	}
 
 	void normalize(Vec3 &v2)
 	{
 		float result;
-		result = sqrt((pow(x, 2)) + (pow(y, 2)) + (pow(z, 2)));
+		result = sqrt(x*x + y*y + z*z);
 		v2.x = x / result;
 		v2.y = y / result;
 		v2.z = z / result;
@@ -78,18 +57,11 @@ public:
 		v2.x = v2.y = v2.z = 0;
 	}
 
-	bool is_zero(Vec3 &v2)
+	bool is_zero(Vec3 &v2) const
 	{
-		if (v2.x == 0 && v2.y == 0 && v2.z == 0) {
-			cout << "v1 is zero ? True" << endl;
-			return true;
-		}
-		else{
-			cout << "v1 is zero ? False" << endl;
-		return false;
+		return (x == v2.x && y == v2.y && z == v2.z);
 	}
-	}
-	Vec3 distance_to(Vec3 &v, Vec3 &v2)
+	Vec3 distance_to(const Vec3 &v2)
 	{
 		Vec3 v4;
 		v4.x = v2.x - v.x;
